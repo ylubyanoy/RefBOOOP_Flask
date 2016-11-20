143af51e35092d6f4423a670402716226e0bc8d5
YLubyanoy <dreif8270@gmail.com>
2016-11-20 16:18:48 +0300

    First commit

---
+++ ref_prj.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('index.html')
+
+    @app.route('/user/<name>')
+    def user(name):
+        return render_template('user.html', name=name)
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('500.html'), 500
+
+    return app
+
+if __name__ == '__main__':
+    create_app().run(debug=True)
